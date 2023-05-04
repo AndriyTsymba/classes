f7301dc407bdb15bfc85130149df69ac60c82e7a
student <artembermnat@gmail.com>
2023-05-04 19:49:38 +0300

    методи класів

--- класи.py
+++ класи.py
@@ -1,12 +1,17 @@
 # теорія класи
 class Student:
+    count = 0
     print("hi!")
-    def __init__(self,height = 150):
+    def __init__(self,height = 150): # кунструктор класу
         self.height = height
+        Student.count += 1
+    def breating(self, height):#  методи класів
+        return self.height - 10
 oleg = Student() # об"єкт б екзимпляр класу
 print(oleg.height)
 masha = Student(height= 200)
 print(masha.height)
-
-
+print(Student.count)
+print(type(1))
+print(masha.breating())
 
